277f0fe63b1bd41f65ee305c82b5cdccde03e6b3
Zhang Di <zhangdi_me@163.com>
2019-07-01 17:18:44 +0800

    Add dotenv support

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+use Dotenv\Dotenv;
+
 defined("BASE_PATH") or define("BASE_PATH", dirname(__DIR__));
 defined("SRC_PATH") or define("SRC_PATH", dirname(__DIR__) . '/src');
 defined("APP_BASE_PATH") or define("APP_BASE_PATH", dirname(__DIR__) . '/src/App');
@@ -7,6 +9,12 @@ defined("VENDOR_PATH") or define("VENDOR_PATH", dirname(__DIR__) . '/vendor');
 defined("RUNTIME_PATH") or define("RUNTIME_PATH", BASE_PATH . '/runtime');
 
 require(VENDOR_PATH . '/autoload.php');
+
+$dotEnvFile = BASE_PATH.'/.env';
+if(file_exists($dotEnvFile)){
+    $dotEnv = Dotenv::create(BASE_PATH);
+    $dotEnv->load();
+}
 
 define("YII_DEBUG", true);
 define("YII_ENV", 'dev');
