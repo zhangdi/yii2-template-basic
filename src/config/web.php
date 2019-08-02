948421021009266a98c7b8b26b7a79edbd3086b9
张迪 <zhangdi_me@163.com>
2019-08-02 22:01:19 +0800

    Update web.php

--- src/config/web.php
+++ src/config/web.php
@@ -32,7 +32,7 @@ $config = [
     ]
 ];
 
-if (!YII_ENV_DEV) {
+if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
